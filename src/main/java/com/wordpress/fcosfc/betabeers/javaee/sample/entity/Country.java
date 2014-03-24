482d9f98326e4f2d8ae2a88ee0d7e9c168e33406
Paco <Paco@localhost>
2014-03-24 18:58:43 +0100

    Small changes on entities

--- src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/entity/Country.java
+++ src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/entity/Country.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Version;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
