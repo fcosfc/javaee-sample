ab8e2e64d5caa7c53d4d465796a83c40a0aa40d6
paco <paco@vbrt01>
2014-08-17 19:42:31 +0200

    Small changes

--- src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/control/Ships.java
+++ src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/control/Ships.java
@@ -10,7 +10,14 @@ import com.wordpress.fcosfc.betabeers.javaee.sample.facade.ShipFacade;
 import com.wordpress.fcosfc.betabeers.javaee.sample.facade.ShipTypeFacade;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
 
 import javax.inject.Inject;
 
@@ -37,7 +44,6 @@ public class Ships extends AbstractController<Ship> implements Serializable{
     
     private List<Country> allCountries;
     private List<ShipType> allShipTypes;
-    private String flag, shipType;
     
     public Ships() {
         super(Ship.class);
@@ -65,58 +71,73 @@ public class Ships extends AbstractController<Ship> implements Serializable{
     public List<ShipType> getAllShipTypes() {
         return allShipTypes;
     }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
-
-    public String getShipType() {
-        return shipType;
-    }
-
-    public void setShipType(String shipType) {
-        this.shipType = shipType;
-    }
     
-    @Override
-    public String create() {
-        try {
-            getCurrentEntity().setFlag(countryFacade.find(flag));
-            getCurrentEntity().setShipType(shipTypeFacade.find(shipType));
-            
-            super.create();
-        } catch (Exception ex) {
-            manageException(ex);
+    @FacesConverter(forClass = ShipType.class)
+    public static class ShipTypeConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext context, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+
+            try {
+                Ships ships = (Ships) context.getApplication().getELResolver().
+                        getValue(context.getELContext(), null, "ships");
+
+                return ships.getShipType(value);
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, null, ex);
+                throw new ConverterException(new FacesMessage(value + "not valid"), ex);
+            }
         }
 
-        return null;
-    }
-    
-    @Override
-    public String prepareEdit() {
-        super.prepareEdit();
-        flag = getCurrentEntity().getFlag().getIsoCode();
-        shipType = getCurrentEntity().getShipType().getShipTypeCode();
+        @Override
+        public String getAsString(FacesContext context, UIComponent component, Object value) {
+            if (value == null) {
+                return null;
+            }
 
-        return null;
+            if (value instanceof ShipType) {
+                return ((ShipType) value).getShipTypeCode();
+            } else {
+                return null;
+            }
+        }
     }
     
-    @Override
-    public String update() {
-        try {
-            getCurrentEntity().setFlag(countryFacade.find(flag));
-            getCurrentEntity().setShipType(shipTypeFacade.find(shipType));
-            
-            super.update();
-        } catch (Exception ex) {
-            manageException(ex);
+    @FacesConverter(forClass = Country.class)
+    public static class CountryConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext context, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+
+            try {
+                Ships ships = (Ships) context.getApplication().getELResolver().
+                        getValue(context.getELContext(), null, "ships");
+
+                return ships.getCountry(value);
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, null, ex);
+                throw new ConverterException(new FacesMessage(value + "not valid"), ex);
+            }
         }
 
-        return null;
+        @Override
+        public String getAsString(FacesContext context, UIComponent component, Object value) {
+            if (value == null) {
+                return null;
+            }
+
+            if (value instanceof Country) {
+                return ((Country) value).getIsoCode();
+            } else {
+                return null;
+            }
+        }
     }
     
     @Override
@@ -125,5 +146,24 @@ public class Ships extends AbstractController<Ship> implements Serializable{
         allCountries = countryFacade.findAll();
         allShipTypes = shipTypeFacade.findAll();
     }
+
+    protected ShipType getShipType(String shipTypeCode) {
+        try {
+            return shipTypeFacade.find(shipTypeCode);                    
+        }  catch (Exception ex) {
+            manageException(ex);
+        }
+
+        return null;
+    }
     
+    protected Country getCountry(String isoCode) {
+        try {
+            return countryFacade.find(isoCode);                    
+        }  catch (Exception ex) {
+            manageException(ex);
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/validation/ImoCodeValidator.java
+++ src/main/java/com/wordpress/fcosfc/betabeers/javaee/sample/validation/ImoCodeValidator.java
@@ -5,36 +5,38 @@ import javax.validation.ConstraintValidatorContext;
 
 /**
  * Implementation for the custom validator example.
- * 
+ *
  * Implementación para el ejemplo de validador personalizado.
- * 
+ *
  * @author Paco Saucedo
  * @see http://en.wikipedia.org/wiki/IMO_numbers
  */
 public class ImoCodeValidator implements ConstraintValidator<ImoCode, Integer> {
-    
+
     @Override
     public void initialize(ImoCode constraintAnnotation) {
     }
-    
+
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
-        String stringValue = value.toString();
-        
-        if (stringValue.length() == 7) {
-            int result = 0;
-            String stringResult;
-            
-            for (int i = 0, multiplier = 7; i < 6; i++, multiplier--) {
-                String v = stringValue.substring(i, i + 1);
-                result += Integer.parseInt(stringValue.substring(i, i + 1)) * multiplier;
+        if (value != null) {
+            String stringValue = value.toString();
+
+            if (stringValue.length() == 7) {
+                int result = 0;
+                String stringResult;
+
+                for (int i = 0, multiplier = 7; i < 6; i++, multiplier--) {
+                    String v = stringValue.substring(i, i + 1);
+                    result += Integer.parseInt(stringValue.substring(i, i + 1)) * multiplier;
+                }
+
+                stringResult = Integer.toString(result);
+
+                return stringValue.substring(6).equals(stringResult.substring(stringResult.length() - 1));
             }
-            
-            stringResult = Integer.toString(result);
-            
-            return stringValue.substring(6).equals(stringResult.substring(stringResult.length() - 1));
         }
-        
+
         return false;
     }
 }
