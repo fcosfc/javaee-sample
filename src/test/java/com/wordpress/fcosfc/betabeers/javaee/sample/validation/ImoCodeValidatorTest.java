347a3166202d8d8b3506b895129080396b2fe8c1
Paco Saucedo <fcosaucedo@gmail.com>
2017-12-31 14:21:06 +0100

    Improvement to the Imo Code Validator Test

--- src/test/java/com/wordpress/fcosfc/betabeers/javaee/sample/validation/ImoCodeValidatorTest.java
+++ src/test/java/com/wordpress/fcosfc/betabeers/javaee/sample/validation/ImoCodeValidatorTest.java
@@ -2,6 +2,7 @@ package com.wordpress.fcosfc.betabeers.javaee.sample.validation;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  * Unit test for the IMO Code Validator
@@ -15,36 +16,38 @@ public class ImoCodeValidatorTest {
     private static final int VALID_IMO_NUMBER = 9043952;
     private static final int WRONG_IMO_NUMBER = 9043954;
     private static final int TOO_SMALL_IMO_NUMBER = 123;
-    private static final int TOO_LONG_IMO_NUMBER = 904395478;
+    private static final int TOO_BIG_IMO_NUMBER = 904395478;
+    
+    private ImoCodeValidator validator;
+    
+    @Before
+    public void createValidator() {
+        validator = new ImoCodeValidator();
+    }
     
     @Test
-    public void testIsValid() {
-        ImoCodeValidator validator = new ImoCodeValidator();
+    public void forAValidIMO_returnTrue() {
         assertTrue(validator.isValid(VALID_IMO_NUMBER, null));
     }
     
     @Test
-    public void testIsNotValid() {
-        ImoCodeValidator validator = new ImoCodeValidator();
+    public void forAWrongIMO_returnFalse() {
         assertFalse(validator.isValid(WRONG_IMO_NUMBER, null));
     }
     
     @Test
-    public void testNullIsNotValid() {
-        ImoCodeValidator validator = new ImoCodeValidator();
+    public void whenNull_returnFalse() {
         assertFalse(validator.isValid(null, null));
     }
     
     @Test
-    public void testTooSmallNumbersAreNotValid() {
-        ImoCodeValidator validator = new ImoCodeValidator();
+    public void forATooSmallNumber_returnFalse() {
         assertFalse(validator.isValid(TOO_SMALL_IMO_NUMBER, null));
     }
     
     @Test
-    public void testTooLongNumbersAreNotValid() {
-        ImoCodeValidator validator = new ImoCodeValidator();
-        assertFalse(validator.isValid(TOO_LONG_IMO_NUMBER, null));
+    public void forATooBigNumber_returnFalse() {
+        assertFalse(validator.isValid(TOO_BIG_IMO_NUMBER, null));
     }
     
 }
